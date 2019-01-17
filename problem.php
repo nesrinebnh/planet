c3d5a9351448a33279c59c24c879a89e416e0a19
nesrine <bounouhnesrine@gmail.com>
2019-01-18 00:26:35 +0100

    extract problem and solution

---
+++ problem.php
@@ -0,0 +1,28 @@
+<?php
+
+
+try {
+    $mysqli = new mysqli("localhost", "root", "", "bounouhflutter");
+
+            
+            $statement = $mysqli->prepare("select * from problem_solution");
+
+
+            $statement->execute(); // Execute the statement.
+            $result = $statement->get_result(); // Binds the last executed statement as a result.
+            $data = [];
+            while ($row = $result->fetch_assoc())
+            $data[] = $row;
+            echo json_encode($data); 
+
+}
+
+catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+    echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+    echo "Exception Msg: " . $e->getMessage();
+    exit(); // exit and close connection.
+}
+
+?>
+
+   
